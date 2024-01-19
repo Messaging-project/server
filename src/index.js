86bb96fc9121efff14769c4fb82acda54b04a83d
Ian Kamau <kian99564@gmail.com>
2024-01-19 15:18:44 +0300

    configure: mongodb

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
+// const express = require("express");
+// const cors = require("cors");
+// const app = express();
+// const http = require("http");
+// const { Server } = require("socket.io");
+
+// app.use(cors());
+// const server = http.createServer(app);
+
+// const io = new Server(server, {
+//   cors: {
+//     origin: "http://localhost:5173",
+//     methods: ["GET", "POST"],
+//   },
+// });
+
+// io.on("connection", (socket) => {
+//   socket.on("join_room", (room) => {
+//     socket.join(room);
+//   });
+
+//   socket.on("send_message", (message) => {
+//     // socket.broadcast.emit("received_message", message);
+//     socket.to(message.room).emit("received_message", message.message);
+//   });
+// });
+
+// server.listen(3001, () => {
+//   console.log("Server listening on port 3001");
+// });
+
 const express = require("express");
 const cors = require("cors");
-const app = express();
 const http = require("http");
 const { Server } = require("socket.io");
 
+const app = express();
 app.use(cors());
-const server = http.createServer(app);
 
+const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: "http://localhost:5173",
@@ -20,11 +51,11 @@ io.on("connection", (socket) => {
   });
 
   socket.on("send_message", (message) => {
-    // socket.broadcast.emit("received_message", message);
     socket.to(message.room).emit("received_message", message.message);
   });
 });
 
-server.listen(3001, () => {
-  console.log("Server listening on port 3001");
+const PORT = 3001;
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
